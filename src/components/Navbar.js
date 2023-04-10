ae07f21700e8da662590497d65592dc52322dc2e
HUYTRUONG99TH <huytruong99th@outlook.com>
2023-04-10 14:16:49 +0700

    delete unncessary files

---
+++ src/components/Navbar.js
@@ -0,0 +1,18 @@
+import React, {useState} from 'react'
+
+function Navbar() {
+  return (
+    <>
+        <nav className="narbar">
+            <div className="navbar-container">
+                <Link to="/" className="navbar-logo">
+                    MuzicBox <img>
+                </Link>
+
+            </div>
+        </nav>
+    </>
+  )
+}
+
+export default Navbar
