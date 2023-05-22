093a2bea789912927affff0dc1c69092fdeded6e
huytruong99th <huytruong99th@outlook.com>
2023-05-22 10:27:19 +0700

    ok

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -51,7 +51,7 @@ function CardItem(props) {
                 <div className='cards__item__list__info'>
                         <h5 className='cards__item__list__text'>{props.text}</h5>
                         <div>
-                            { /*locationPermission &&*/ props.distance !== null ? <div className='cards__item__list__distance'>
+                            { /*locationPermission && props.distance !== null*/ true ? <div className='cards__item__list__distance'>
                                 <svg xmlns="http://www.w3.org/2000/svg" height="28" viewBox="0 0 64 64" width="28"><g id="Pin"><path d="m32 0a24.0319 24.0319 0 0 0 -24 24c0 17.23 22.36 38.81 23.31 39.72a.99.99 0 0 0 1.38 0c.95-.91 23.31-22.49 23.31-39.72a24.0319 24.0319 0 0 0 -24-24zm0 35a11 11 0 1 1 11-11 11.0066 11.0066 0 0 1 -11 11z"/></g></svg>
                                 <p>{props.distance}</p>
                             </div>
